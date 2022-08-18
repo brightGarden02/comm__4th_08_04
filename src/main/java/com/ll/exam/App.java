2835a6f8436fedde2daeef9aafab2551de261e7e
brightGarden02 <rreone2017@gmail.com>
2022-08-18 15:49:33 +0900

    feat: 운영용 db 접속정보 추가, 톰캣7 메이븐플러그인 배포 설정

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,10 +1,26 @@
 package com.ll.exam;
 
 public class App {
-    public static final String BASE_PACKAGE_PATH = "com.ll.exam";
-    public static final String DB_HOST = "localhost";
-    public static final int DB_PORT = 3306;
-    public static final String DB_ID = "lldj";
-    public static final String DB_PASSWORD = "lldj1234";
-    public static final String DB_NAME = "blog";
+    public static String DB_HOST = "localhost";
+    public static int DB_PORT = 3306;
+    public static String DB_ID = "lldj";
+    public static String DB_PASSWORD = "lldj1234";
+    public static String DB_NAME = "blog";
+    public static boolean isProd = false;
+
+    static {
+        String profilesActive = System.getenv("SPRING_PROFILES_ACTIVE");
+
+        if (profilesActive != null && profilesActive.equals("production")) {
+            isProd = true;
+        }
+
+        if (isProd) {
+            DB_HOST = "172.17.0.1";
+            DB_PORT = 3306;
+            DB_ID = "lldj";
+            DB_PASSWORD = "lldj123414";
+            DB_NAME = "site1";
+        }
+    }
 }
