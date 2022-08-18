15133f214ef7f41426df7f395116f07a01bbdd1c
brightGarden02 <rreone2017@gmail.com>
2022-08-18 13:15:07 +0900

    feat: 각각의 테스트케이스들이 독립적인 환경에서 실행되는 것을 보장하기 위해 beforeEach 도입

--- src/test/java/com/ll/exam/ArticleServiceTest.java
+++ src/test/java/com/ll/exam/ArticleServiceTest.java
@@ -2,13 +2,62 @@ package com.ll.exam;
 
 import com.ll.exam.article.service.ArticleService;
 import com.ll.exam.dto.ArticleDto;
+import com.ll.exam.mymap.MyMap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArticleServiceTest {
+
+    // @BeforeEach를 붙인 아래 메서드는
+    // @Test가 달려있는 메서드가 실행되기 전에 자동으로 실행이 된다.
+    // 주로 테스트 환경을 깔끔하게 정리하는 역할을 한다.
+    // 즉 각각의 테스트케이스가 독립적인 환경에서 실행될 수 있도록 하는 역할을 한다.
+    @BeforeEach
+    public void beforeEach() {
+
+        // 게시물 테이블을 깔끔하게 삭제한다.
+        // DELETE FROM article; // 보다 TRUNCATE article; 로 삭제하는게 더 깔끔하고 흔적이 남지 않는다.
+        truncateArticleTable();
+
+        // 게시물 3개를 만든다.
+        // 테스트에 필요한 샘플데이터를 만든다고 보면 된다.
+        makeArticleTestData();
+        
+    }
+
+    private void makeArticleTestData() {
+
+        MyMap myMap = Container.getObj(MyMap.class);
+
+        IntStream.rangeClosed(1, 3).forEach(no -> {
+            boolean isBlind = false;
+            String title = "제목%d".formatted(no);
+            String body = "내용%d".formatted(no);
+
+            myMap.run("""
+                    INSERT INTO article
+                    SET createDate = NOW(),
+                    modifiedDate = NOW(),
+                    title = ?,
+                    `body` = ?,
+                    isBlind = ?
+                    """, title, body, isBlind);
+        });
+
+    }
+
+    private void truncateArticleTable() {
+        MyMap myMap = Container.getObj(MyMap.class);
+
+        // 테이블을 깔끔하게 지워준다.
+        myMap.run("TRUNCATE article");
+    }
+
 
     @Test
     public void 존재한다() {
